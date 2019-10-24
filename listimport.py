5d1ed58c875f6e3ac2bfe40e40aa2889ed14cf2f
Pascal Cotret <pascal.cotret@ensta-bretagne.fr>
2019-10-24 09:07:35 +0200

    gestion ISBN inconnu

--- listimport.py
+++ listimport.py
@@ -13,26 +13,31 @@ if __name__ == '__main__':
     # Open file in read mode
     isbn_file = open("input.txt", 'r')
     md_file = open('markdown.md', 'w+')
+    not_found = open('not_found.txt', 'w+')
     # Read everything and put it in a list
     mylist = isbn_file.read().splitlines()
-    print(mylist)
     md_str = '|Title' + '|Author' + '|Publisher' + '|Year' + '|ISBN|\n' \
            + '|-----' + '|------' + '|---------' + '|----' + '|----|\n'
     md_file.write(md_str)
     # For each element of the list
     for index in range(len(mylist)):
-        meta_dict = isbnlib.meta(str(mylist[index]))
-        print(meta_dict)
-        # Part 2 - Modify authors field from meta_dict to get lastname/firstname
-        # of the 1st author. I would say something like:
-        first_author = meta_dict['Authors'][0].split()
-        md_str = '|' +  meta_dict['Title'] \
-               + '|' +  first_author[1] + ", " + first_author[0] \
-               + '|' +  meta_dict['Publisher'] \
-               + '|' +  meta_dict['Year'] \
-               + '|' +  '['+ meta_dict['ISBN-13'] + '](http://isbndb.com/book/' + meta_dict['ISBN-13'] + ')|\n'
-        print(md_str)
-        md_file.write(md_str)
+        print('Searching a book with ISBN ' + str(mylist[index]) + '...')
+        try:
+            meta_dict = isbnlib.meta(str(mylist[index]))
+            print('Adding the book ' + meta_dict['Title'] + ' by ' + meta_dict['Authors'][0].split()[1])
+            # Part 2 - Modify authors field from meta_dict to get lastname/firstname
+            # of the 1st author. I would say something like:
+            first_author = meta_dict['Authors'][0].split()
+            md_str = '|' + meta_dict['Title'] \
+                     + '|' + first_author[1] + ", " + first_author[0] \
+                     + '|' + meta_dict['Publisher'] \
+                     + '|' + meta_dict['Year'] \
+                     + '|' + '[' + meta_dict['ISBN-13'] + '](http://isbndb.com/book/' + meta_dict['ISBN-13'] + ')|\n'
+            md_file.write(md_str)
+        except Exception:
+            print('Book not found!')
+            not_found.write(str(mylist[index])+'\n')
     # Done, closes the files
     isbn_file.close()
     md_file.close()
+    not_found.close()
--- singleimport.py
+++ singleimport.py
@@ -15,17 +15,22 @@ if __name__ == '__main__':
     # Read everything and put it in a list
     isbn_number = sys.argv[1]
     # Print its ISBN info and write to the bibfile
-    meta_dict = isbnlib.meta(str(isbn_number))
-    # Part 2 - Modify authors field from meta_dict to get lastname/firstname
-    # of the 1st author. I would say something like:
-    first_author = meta_dict['Authors'][0].split()
-    md_str = '|Title' + '|Author' + '|Publisher' + '|Year' + '|ISBN|\n' \
-           + '|-----' + '|------' + '|---------' + '|----' + '|----|\n' \
-           + '|' +  meta_dict['Title'] \
-           + '|' +  first_author[1] + ", " + first_author[0] \
-           + '|' +  meta_dict['Publisher'] \
-           + '|' +  meta_dict['Year'] \
-           + '|' +  '['+ meta_dict['ISBN-13'] + '](http://isbndb.com/book/' + meta_dict['ISBN-13'] + ')|'
-    md_file.write(md_str)
+    print('Searching a book with ISBN ' + str(isbn_number)+'...')
+    try:
+        meta_dict = isbnlib.meta(str(isbn_number))
+        print('Adding the book ' + meta_dict['Title'] + ' by ' + meta_dict['Authors'][0].split()[1])
+        # Part 2 - Modify authors field from meta_dict to get lastname/firstname
+        # of the 1st author. I would say something like:
+        first_author = meta_dict['Authors'][0].split()
+        md_str = '|Title' + '|Author' + '|Publisher' + '|Year' + '|ISBN|\n' \
+               + '|-----' + '|------' + '|---------' + '|----' + '|----|\n' \
+               + '|' +  meta_dict['Title'] \
+               + '|' +  first_author[1] + ", " + first_author[0] \
+               + '|' +  meta_dict['Publisher'] \
+               + '|' +  meta_dict['Year'] \
+               + '|' +  '['+ meta_dict['ISBN-13'] + '](http://isbndb.com/book/' + meta_dict['ISBN-13'] + ')|'
+        md_file.write(md_str)
+    except Exception:
+        print('Book not found!')
     # Done, closes the files
     md_file.close()
